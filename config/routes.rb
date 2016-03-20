c8f317ea4f8b75ed36a78b851e8614a20e442f07
thminhVN <thminhkg@gmail.com>
2016-03-20 22:41:28 +0700

    Finish register and check session in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :users
+  get 'register' => 'users#new'
+  get 'login' => 'users#login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
